d87156c33455a110b9e9e860ced636044a324405
mAyush24 <ayush2524mishra@gmail.com>
2026-04-29 02:00:10 +0530

    Fix model loading issue

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load files
-model = load_model("lstm_model.h5")
+# model = load_model("lstm_model.h5")
+model = load_model("lstm_model.h5", compile=False)
 
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
